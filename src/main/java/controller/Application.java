c868ecd9bb64f00d15c7aaeef30f05f072eff2dd
Rafael Possenti <rafael_possenti@hotmail.com>
2017-05-02 20:33:24 -0300

    Type: Custom - add the index page and make the route

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -2,11 +2,8 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
@@ -14,9 +11,6 @@ public class Application {
 
 	}
 	
-	@RequestMapping(value="/")
-	public String Demo() {
-		return "index";
-	}
+	
 
 }
---
+++ src/main/java/controller/dashboardController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class dashboardController {
+
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
